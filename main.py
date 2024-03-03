2ce51b3011a0ceb670ee68af7c9ae74dd1038d58
KenHuang <ken103023@gmail.com>
2024-03-03 17:55:44 +0800

    use pyinstaller build exe file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+import webbrowser as web
+import requests
+
+web.open("http://192.168.1.109:30038")
+
+
+user_search = input("input you want to search at google search : ")
+web.open("https://www.google.com/search?q=" + user_search)
+'''web.open("http://192.168.1.109")'''
+
---
+++ pythonexefile/openNasBT.py
@@ -0,0 +1,6 @@
+'''open nas bt downloader'''
+import webbrowser as web
+
+web.open("http://192.168.1.109:30038")
+print("open successful")
+
